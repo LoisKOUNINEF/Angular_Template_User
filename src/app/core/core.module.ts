f8f458ac92e23947ef60b9052fd31c85255b4601
Lois KOUNINEF <lkouninef@gmail.com>
2023-02-07 10:52:48 +0100

    remove unused import

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { ApiService } from './services/api/api.service';
-import { AuthService } from './services/auth/auth.service';
 import { SharedModule } from '../shared/shared.module';
-
-
 
 @NgModule({
   declarations: [],
-  imports: [SharedModule,
+  imports: [
+    SharedModule,
   ],
   providers: [
     ApiService,
-    AuthService,
   ],
 })
 export class CoreModule { }
