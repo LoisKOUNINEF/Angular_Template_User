2887db8d2314a7c7e3ff9e8059c72f483a5f6d40
Lois KOUNINEF <lkouninef@gmail.com>
2023-02-08 09:35:03 +0100

    coremodule only importable in root module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { ApiService } from './services/api/api.service';
 import { SharedModule } from '../shared/shared.module';
 
@@ -11,4 +11,10 @@ import { SharedModule } from '../shared/shared.module';
     ApiService,
   ],
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() core: CoreModule) {
+    if(core) {
+      throw new Error('Core module should be imported only in the root module.')
+    }
+  }
+}
